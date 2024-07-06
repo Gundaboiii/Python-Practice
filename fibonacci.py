6fb5fe89490687ac700cbcb99437f914be8660c0
Ronak Dhingra <159307752+Gundaboiii@users.noreply.github.com>
2024-07-06 02:42:14 -0500

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,16 +1,8 @@
-def fibonacci(n):
-    sequence = []
-    a, b = 0, 1
-    for _ in range(n):
-        sequence.append(a)
-        a, b = b, a + b
-    return sequence
+def fibo(n):
+    if n <= 1:
+        return n
+    else:
+        return(fibo(n-1) + fibo(n-2))
 
-# Input number of terms from the user
-n = int(input("Enter the number of terms in the Fibonacci sequence: "))
-
-# Calling the fibonacci function
-fibonacci_sequence = fibonacci(n)
-
-# Printing the Fibonacci sequence
-print("Fibonacci sequence:", fibonacci_sequence)
+n= int(input("Enter a Number: "))
+print(fibo(n))
