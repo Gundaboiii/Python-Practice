266d2dead72be9f4e70910540856247b426ef4ce
Ronak Dhingra <159307752+Gundaboiii@users.noreply.github.com>
2024-07-21 23:06:24 -0500

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -1,20 +1,12 @@
-lst = []
-n = int(input("Enter number of elements : "))
-for i in range(0, n):
-	ele = int(input())
-	lst.append(ele) 
-print(lst)
-target = int(input("Enter Target number to check if two numbers from list add up to this number: "))
-target_lst = []
-for i in range(0,n-1):
-	for j in range(i+1,n):
-		if lst[i] + lst[j] == target:
-			target_lst.append(lst[i])
-			target_lst.append(lst[j])
-			break
-		else:
-			continue
-if target_lst == []:
-	print("No two numbers from list add up to this number")
-else:
-	print(target_lst)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        new_lst = []
+        for i in range(0,len(nums)-1):
+	        for j in range(i+1,len(nums)):
+		        if nums[i] + nums[j] == target:
+			        new_lst.append(i)
+			        new_lst.append(j)
+			        break
+		        else:
+			        continue
+        return new_lst
